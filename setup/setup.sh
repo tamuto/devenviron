0c410b2a1a59e7f958e4320d73d7c4d244a8f213
tamuto <mutomob@gmail.com>
2021-12-25 16:59:40 +0900

    環境統合

---
+++ setup/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir ~/.devenviron
+mkdir ~/.devenviron/mysql_data
+touch ~/.devenviron/.gitconfig
+touch ~/.devenviron/.git-credentials
+touch ~/.devenviron/.awsconfig
+touch ~/.devenviron/.aws-credentials
