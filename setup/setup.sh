04424c0c589b4b287880fcfda4010575dd39ba8d
tamuto <mutomob@gmail.com>
2021-12-27 12:45:14 +0900

    setupシェルの修正

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,8 +8,8 @@ touch ~/.devenviron/.awsconfig
 touch ~/.devenviron/.aws-credentials
 
 mkdir -p ~/bin
-wget -o ~/bin/denv https://raw.githubusercontent.com/tamuto/devenviron/main/setup/bin/denv 
-wget -o ~/bin/denvp https://raw.githubusercontent.com/tamuto/devenviron/main/setup/bin/denvp
-wget -o ~/bin/denvdb https://raw.githubusercontent.com/tamuto/devenviron/main/setup/bin/denvdb
+wget -P ~/bin https://raw.githubusercontent.com/tamuto/devenviron/main/setup/bin/denv 
+wget -P ~/bin https://raw.githubusercontent.com/tamuto/devenviron/main/setup/bin/denvp
+wget -P ~/bin https://raw.githubusercontent.com/tamuto/devenviron/main/setup/bin/denvdb
 
 chmod +x ~/bin/denv*
