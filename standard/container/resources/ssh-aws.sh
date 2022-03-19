6c042a2a98ba4802f4754fad8118e7b4953b70f0
tamuto <mutomob@gmail.com>
2022-03-19 01:46:27 +0000

    awscli v2対応

--- standard/container/resources/ssh-aws.sh
+++ standard/container/resources/ssh-aws.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 instanceId=$(aws --profile $1 ec2 describe-instances --query "Reservations[*].Instances[*].InstanceId" --filters "Name=tag:Name,Values=[$2]" --output text)
-ssh -oProxyCommand="aws --profile $1 ssm start-session --target ${instanceId} --document-name AWS-StartSSHSession --parameters 'portNumber=[22]'" ${@:3} $2
+[[ -z "$instanceId" ]] && { echo "Cannot get instanceId."; exit 1; }
+ssh -oProxyCommand="aws --profile $1 ssm start-session --target ${instanceId} --document-name AWS-StartSSHSession --parameters 'portNumber=[22]'" $2 ${@:3}
