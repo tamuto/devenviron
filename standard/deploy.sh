4e7d50e90e8057a81d2da89b1d03d93c7c9e1b51
tamuto <mutomob@gmail.com>
2022-10-23 08:40:38 +0000

    Dockerfile修正

--- standard/deploy.sh
+++ standard/deploy.sh
@@ -1,2 +1,2 @@
-podman push docker.io/tamuto/devenviron:$1
-podman push docker.io/tamuto/devenviron:latest
+podman push docker.io/tamuto/devenviron:$1 --format v2s2
+podman push docker.io/tamuto/devenviron:latest --format v2s2
