19525f7e29afba16eb832f0588f5eb6891d9f738
tamuto <mutomob@gmail.com>
2023-05-03 03:10:41 +0000

    scriptの方針を変更

--- scripts/build_py39.sh
+++ scripts/build_py39.sh
@@ -2,7 +2,7 @@
 set -eu
 
 mkdir -p build/resources
-sed "s/{{BASEIMG}}/${3:-"python:3.9-slim-bullseye"}/" template/container/Dockerfile.tmpl > build/Dockerfile
+sed "s/{{BASEIMG}}/python:3.9-slim-bullseye/" template/container/Dockerfile.tmpl > build/Dockerfile
 
 cp template/container/resources/* build/resources/
 ${2:-podman} build -t docker.io/tamuto/devenviron:$1 build -f build/Dockerfile
