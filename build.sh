011e0229aefb9f82ea35ef60104304c7ba9f2b83
tamuto <mutomob@gmail.com>
2023-05-03 03:03:01 +0000

    スクリプト修正

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eu
+
+mkdir -p build/resources
+sed "s/{{BASEIMG}}/${3:-"python:3.9-slim-bullseye"}/" template/container/Dockerfile.tmpl > build/Dockerfile
+
+cp template/container/resources/* build/resources/
+${2:-podman} build -t docker.io/tamuto/devenviron:$1 build -f build/Dockerfile
+${2:-podman} tag docker.io/tamuto/devenviron:$1 docker.io/tamuto/devenviron:latest
--- etc/deploy.sh
+++
@@ -1,2 +0,0 @@
-podman push docker.io/tamuto/devenviron:$1 --format v2s2
-podman push docker.io/tamuto/devenviron:latest --format v2s2
