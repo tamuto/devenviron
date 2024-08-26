39e04b8ab381187f26a4352244ce0c28b5c20daf
tamuto <mutomob@gmail.com>
2024-08-26 09:25:33 +0000

    新環境用スクリプト追加

---
+++ scripts/build_allbase_r36.3.0.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+
+mkdir -p build/resources
+cat template/container/Dockerfile.tmpl | sed \
+-e "s/{{BASEIMG}}/allbase:l4t-r36.3.0/" \
+-e "s/{{ARCH}}/aarch64/" \
+-e "s/{{SSM_ARCH}}/ubuntu_arm64/" > build/Dockerfile
+
+cp template/container/resources/* build/resources/
+docker build -t docker.io/tamuto/devenviron:$1 build -f build/Dockerfile
