619de02f0badfcb1fa4ce9d462e0f3f7b8cbd730
tamuto <mutomob@gmail.com>
2025-03-13 05:15:22 +0000

    add script for python3.12

---
+++ scripts/build_py3_12.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+
+mkdir -p build/resources
+cat template/container/Dockerfile.tmpl | sed \
+-e "s/{{BASEIMG}}/python:3.12-slim-bookworm/" \
+-e "s/{{ARCH}}/x86_64/" \
+-e "s/{{SSM_ARCH}}/ubuntu_64bit/" > build/Dockerfile
+
+cp template/container/resources/* build/resources/
+docker build -t docker.io/tamuto/devenviron:$1 build -f build/Dockerfile
+docker tag docker.io/tamuto/devenviron:$1 docker.io/tamuto/devenviron:latest
